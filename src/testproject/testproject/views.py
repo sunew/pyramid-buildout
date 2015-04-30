142ca646d559ca5066a8697d2470fc498be3df47
Sune Broendum Woeller <sune@woeller.dk>
2015-04-30 20:11:42 +0200

    logging

--- src/testproject/testproject/views.py
+++ src/testproject/testproject/views.py
@@ -1,7 +1,10 @@
 from pyramid.view import view_config
 
+import logging
+log = logging.getLogger(__name__)
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
+    log.debug('TESTLOG')
     print  request.registry.settings['buildout_dir']
     return {'project': 'testproject'}
